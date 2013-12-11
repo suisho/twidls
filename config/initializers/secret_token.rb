409c10137a316c820b19bfee8241e7cbf1ccdfdc
suisho <suisho@suisho-no-MacBook-Air.local>
2013-12-11 23:52:23 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twidls::Application.config.secret_key_base = '90149c7cfc8e245e2e43c971184d29534022b1ebe0e8df736412afe9e0473513529e3aad48c60d84652cb07cc72fb97a404a7f7df03331ebcc2db588fccf4b47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twidls::Application.config.session_store :cookie_store, key: '_twidls_session'
