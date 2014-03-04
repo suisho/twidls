3126b0f271ebcd5704dffa3d46f5eb600c2b2a5c
suisho <suisho000000@gmail.com>
2014-03-05 01:01:10 +0900

    drag dropのmocking

---
+++ prototype/app/js/power.js
@@ -0,0 +1,31 @@
+(function($){
+  if (!Modernizr.draganddrop) {
+    console.log("やばいよやばいよ")
+  }
+  var dragged = undefined;
+  // drag & drop
+  $(document).on("dragover", function(e){
+    e.preventDefault();
+  }).on('dragenter', function(e){
+    $(e.target).addClass("dragenter")
+  }).on('dragleave', function(e){
+    $(e.target).removeClass("dragenter")
+  }).on('dragstart', function(e){
+    dragged = e.target
+  }).on('drop', function(e){
+    e.preventDefault();
+    
+    var $target = $(e.target)
+    if(!$target.hasClass("dropzone")){
+      return false
+    }
+
+    var $append = $(dragged)
+    if($(dragged).parent().hasClass("item_master")){
+      $append = $(dragged).clone()
+    }
+    $target.append($append)
+    $(e.target).removeClass("dragenter")
+    return false
+  })
+})(jQuery)
