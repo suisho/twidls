dc0deba8aae6f4e892f924144f4ddb34d9ebe6f5
suisho <suisho000000@gmail.com>
2014-03-05 23:46:13 +0900

    add stylus

--- prototype/gulpfile.js
+++ prototype/gulpfile.js
@@ -2,6 +2,8 @@ var gulp  = require('gulp')
 var gulpFilter = require('gulp-filter')
 var bower = require('gulp-bower-files');
 var clean = require('gulp-clean');
+var stylus = require('gulp-stylus')
+var concat = require('gulp-concat')
 
 gulp.task('bower', function() {
   var dest = 'lib/'
@@ -18,4 +20,18 @@ gulp.task('bower', function() {
 
 });
 
-gulp.task('default', ['bower'])
+gulp.task('stylus', function(){
+  var dest = "./app/css"
+
+ gulp.src(dest)
+    .pipe(clean());
+  
+  gulp.src('./stylus/**/*.styl')
+    .pipe(stylus({
+      //set:['compress']
+    }))
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest(dest))
+})
+
+gulp.task('default', ['bower', 'stylus'])
